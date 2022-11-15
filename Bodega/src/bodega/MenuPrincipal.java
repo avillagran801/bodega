0c5ade6ef1d33d59f022065bc90a4511e973ea0b
Jose Cortes Pizarro <joscortes2021@udec.cl>
2022-11-15 11:09:43 -0300

    Creación de Menu Principal.

--- Bodega/src/bodega/MenuPrincipal.java
+++ Bodega/src/bodega/MenuPrincipal.java
@@ -4,6 +4,47 @@ import javax.swing.*;
 
 public class MenuPrincipal extends JPanel{
     
+    private VentanaPedido window;
     
-    //public 
+    public MenuPrincipal (int anchoVentana, int largoVentana) {
+        this.setLayout(null);
+        this.setBounds(0, 0, anchoVentana, largoVentana);
+        this.setBackground(Color.BLUE);
+        botones();
+    }
+    
+    public void setWindow (VentanaPedido window_aux){
+        window = window_aux;
+    }
+    
+    private void botones() {
+        JButton inicio = new JButton("Iniciar Pedido");
+        inicio.setBounds((this.getWidth()/2)-200, (this.getHeight()/2)-200, 200, 200);
+        //inicio.setBounds(this.getWidth()-((this.getWidth()/2)-200), 
+                //this.getHeight()-((this.getHeight()/2)-300), 200, 300);
+        inicio.setBorderPainted(false);
+        inicio.setContentAreaFilled(false);
+        inicio.setForeground(Color.white);        
+        inicio.setFont(new Font("SansSerif.", 1, 16));
+        
+        inicio.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                window.setComponentZOrder(window.getMenu(), 3);
+                window.setComponentZOrder(window.getSelector(), 0);
+                window.setComponentZOrder(window.getResumen(), 1);
+                window.setComponentZOrder(window.getBase(), 2);
+                
+                window.getMenu().setVisible(false);
+                
+                window.getSelector().setVisible(true);
+                window.getResumen().setVisible(true);
+                window.getBase().setVisible(true);
+                
+                repaint();
+            }
+        });
+        
+        this.add(inicio);
+    }
 }
--- Bodega/src/bodega/VentanaPedido.java
+++ Bodega/src/bodega/VentanaPedido.java
@@ -7,6 +7,7 @@ public class VentanaPedido extends JFrame {
     private SelectorCategorias selCat;
     private ResumenPedido res;
     private BasePedidos base;
+    private MenuPrincipal principal;
 
     public VentanaPedido() {
         super();
@@ -22,13 +23,43 @@ public class VentanaPedido extends JFrame {
         selCat = new SelectorCategorias(this.getWidth(), this.getHeight());
         res = new ResumenPedido(this.getWidth(), this.getHeight());
         base = new BasePedidos(this.getWidth(), this.getHeight());
+        principal = new MenuPrincipal(this.getWidth(), this.getHeight());
+        
+        setWindowInComponents();
         
         this.getContentPane().add(selCat);
         this.getContentPane().add(res);
         this.getContentPane().add(base);
+        this.getContentPane().add(principal);
         
-        this.setComponentZOrder(selCat, 0);
-        this.setComponentZOrder(res, 1);
-        this.setComponentZOrder(base, 2);
+        this.principal.setVisible(true);
+        this.selCat.setVisible(false);
+        this.base.setVisible(false);
+        this.res.setVisible(false);
+        
+        this.setComponentZOrder(principal, 0);
+        this.setComponentZOrder(selCat, 1);
+        this.setComponentZOrder(res, 2);
+        this.setComponentZOrder(base, 3);
+    }
+    
+    private void setWindowInComponents(){
+        principal.setWindow(this);
+    }
+    
+    public MenuPrincipal getMenu(){
+        return principal;
+    }
+    
+    public SelectorCategorias getSelector(){
+        return selCat;
+    }
+    
+    public ResumenPedido getResumen(){
+        return res;
+    }
+    
+    public BasePedidos getBase(){
+        return base;
     }
 }
